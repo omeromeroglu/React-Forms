e8ad5d888499adb3f3b7953c596961a3cb804744
OMER FARUK OMEROGLU <omeromeroglu95@gmail.com>
2021-11-30 23:52:34 +0300

    React Forms

---
+++ src/components/SimpleInput.js
@@ -0,0 +1,51 @@
+import { useRef, useState } from "react";
+
+const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredNameValid,setEnteredNameValid] =useState(true);
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+
+    if (enteredName.trim() === "") {
+      setEnteredNameValid(false);
+      return;
+    }
+
+    
+
+    console.log(enteredName);
+    console.log(nameInputRef.current.value);
+
+    setEnteredName("");
+  };
+
+  const nameInputClasses = enteredNameValid ? "form-control" : "form-control invalid";
+
+  return (
+    <form onSubmit={formSubmissionHandler}>
+      <div className={nameInputClasses}>
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+          value ={enteredName}
+        />
+
+        {!enteredNameValid &&<p className="error-text">Name must not be empy</p>}
+      </div>
+      <div className="form-actions">
+        <button>Submit</button>
+      </div>
+    </form>
+  );
+};
+
+export default SimpleInput;
